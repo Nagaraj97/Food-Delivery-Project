7d8543d52fca81d2d4b34e53ada90a89ef44d18a
Nagaraj GN <nagarajgn7911@gmail.com>
2024-08-07 17:49:57 +0530

    first connit

---
+++ Frontend/src/components/Layout/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+
+  <footer>
+        <p className="text-center">
+            Food Delivery Website -2023-2024, All rights reserved
+        </p>
+        </footer>
+      
+    </>
+  )
+}
+
+export default Footer
